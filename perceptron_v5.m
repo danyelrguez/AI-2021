bac1f048df5fbe222d0ed5d1e354ff803392f10d
danyelrguez <89834053+danyelrguez@users.noreply.github.com>
2021-09-02 00:33:16 -0500

    Update perceptron_v5.m
    weight line mod

--- perceptron_v5.m
+++ perceptron_v5.m
@@ -19,26 +19,38 @@ d = [];
 for i=1:n
     X(i,1) = input('Data Class 1 in coord x (-5 to 5) \n');
     if or(X(i,1)>5,X(i,1)<-5)
-        error('Out of range')
+        disp('Out of range')
+        while(or(X(i,1)>5,X(i,1)<-5))
+            X(i,1) = input('Data Class 1 in coord x (-5 to 5) \n');
+        end
     end
     X(i,2) = input('Data Class 1 in coord y (-5 to 5) \n');
     if or(X(i,1)>5,X(i,1)<-5)
-        error('Out of range')
+        disp('Out of range')
+        while(or(X(i,1)>5,X(i,1)<-5))
+            X(i,1) = input('Data Class 1 in coord y (-5 to 5) \n');
+        end
     end
     d(i) = 1;
 end
 for i=n+1:n+n
     X(i,1) = input('Data Class 2 in coord x (-5 to 5) \n');
     if or(X(i,1)>5,X(i,1)<-5)
-        error('Out of range')
+                disp('Out of range')
+        while(or(X(i,1)>5,X(i,1)<-5))
+            X(i,1) = input('Data Class 2 in coord x (-5 to 5) \n');
+        end
     end
     X(i,2) = input('Data Class 2 in coord y (-5 to 5) \n');
     if or(X(i,1)>5,X(i,1)<-5)
-        error('Out of range')
+                disp('Out of range')
+        while(or(X(i,1)>5,X(i,1)<-5))
+            X(i,1) = input('Data Class 2 in coord y (-5 to 5) \n');
+        end
     end
     d(i) = -1;
 end
-
+d = d';
 clc;
 
 figure()
@@ -48,7 +60,16 @@ ylim([-5.5 5.5])
 
 % X = [X,ones(length(d),1)];
 %Random initial weights and display them
-w = rand(size(X,2),1);
+w = rand(2,1);
+% w = w';
+theta = input('Input threshold theta (-5 to 5) \n');
+    if or(theta>5,theta<-5)
+        disp('Out of range')
+        while(or(theta>5,theta<-5))
+            theta = input('Input threshold theta (-5 to 5) \n');
+        end
+    end
+% theta = rand();
 disp('Initial random weight vector is:');
 disp(w');
 
@@ -58,7 +79,7 @@ lr = input('Learning rate \n');
 epochs = input('Number of max epochs \n');
 
 kk=0;
-
+% w*X1'
 %Training 
 while(1)
     error=0;
@@ -71,6 +92,7 @@ while(1)
         error=error+err;
         % Weight change using perceptron rule
         w=w+lr*err*x;
+        theta=theta+lr*err;
         kk=kk+1;
         
         scatter(X(:,1),X(:,2),[],d,'filled')
@@ -78,18 +100,20 @@ while(1)
         ylim([-5.5 5.5])
         hold on
         xw = -5.5:0.01:5.5;
-        yw = -(w(length(w)-1)/w(length(w)))*xw - (w(length(w))/w(length(w)-1));
+        yw = (w(1)/w(2))*xw + (theta/w(2));
         scatter(xw,yw,'.');
         hold off
         drawnow
-        pause(0.4)
+        pause(1)
     end
     if (error==0)
         disp('Convergence')
         disp('Weight');
         disp(w');
+        disp('Theta');
+        disp(theta);
         disp('Epochs');
-        disp(epochs);
+        disp(kk);
         disp('Error');
         disp(error);
         break;
@@ -98,8 +122,10 @@ while(1)
         disp('No convergence')
         disp('Weight',w);
         disp(w);
+        disp('Theta');
+        disp(theta);
         disp('Epochs');
-        disp(epochs);
+        disp(kk);
         disp('Error');
         disp(error);
         break;
